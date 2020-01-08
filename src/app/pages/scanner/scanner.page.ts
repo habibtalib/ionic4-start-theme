2ca82c0c549af9166f47b515f37f300ca003b715
Habib <habib.talib@imoney-group.com>
2020-01-08 09:20:07 +0800

    revert scanner

--- src/app/pages/scanner/scanner.page.ts
+++ src/app/pages/scanner/scanner.page.ts
@@ -1,34 +1,29 @@
-import { Component, OnInit, ChangeDetectorRef } from "@angular/core";
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import {
   NavController,
   AlertController,
   MenuController,
   ToastController,
   ActionSheetController,
-  Platform,
-  LoadingController
-} from "@ionic/angular";
-import { AuthService } from "src/app/services/auth.service";
+  Platform, LoadingController
+} from '@ionic/angular';
+import { AuthService } from 'src/app/services/auth.service';
 import { EnvService } from "../../services/env.service";
-import { BarcodeScanner } from "@ionic-native/barcode-scanner/ngx";
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { tap } from "rxjs/operators";
-import { ThrowStmt } from "@angular/compiler";
-import { File, FileEntry } from "@ionic-native/File/ngx";
-import { WebView } from "@ionic-native/ionic-webview/ngx";
-import { Storage } from "@ionic/storage";
-import { FilePath } from "@ionic-native/file-path/ngx";
-import {
-  Camera,
-  CameraOptions,
-  PictureSourceType
-} from "@ionic-native/Camera/ngx";
-import { finalize } from "rxjs/operators";
-import { AlertService } from "src/app/services/alert.service";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { NgForm } from "@angular/forms";
+import { tap } from 'rxjs/operators';
+import { ThrowStmt } from '@angular/compiler';
+import { File, FileEntry } from '@ionic-native/File/ngx';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
+import { Storage } from '@ionic/storage';
+import { FilePath } from '@ionic-native/file-path/ngx';
+import { Camera, CameraOptions, PictureSourceType } from '@ionic-native/Camera/ngx';
+import { finalize } from 'rxjs/operators';
+import { AlertService } from 'src/app/services/alert.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
-const STORAGE_KEY = "scanner";
+const STORAGE_KEY = 'scanner';
 
 @Component({
   selector: "app-scanner",
@@ -42,30 +37,31 @@ export class ScannerPage implements OnInit {
   images = [];
   member: any;
   user: any;
-  name: any;
-  address: any;
-  phone: any;
-  email: any;
+  name = "";
+  address = "";
+  phone = "";
+  email = "";
   formData = new FormData();
+  
 
   constructor(
     private authService: AuthService,
     public navCtrl: NavController,
-    private env: EnvService,
+    private env: EnvService, 
     public menuCtrl: MenuController,
     public barcodescanner: BarcodeScanner,
     private http: HttpClient,
     private toastController: ToastController,
-    private storage: Storage,
-    private plt: Platform,
+    private storage: Storage, 
+    private plt: Platform, 
     private loadingController: LoadingController,
-    private ref: ChangeDetectorRef,
+    private ref: ChangeDetectorRef, 
     private filePath: FilePath,
-    private camera: Camera,
+    private camera: Camera, 
     private file: File,
     private webview: WebView,
-    private actionSheetController: ActionSheetController,
-    private alertService: AlertService
+    private actionSheetController: ActionSheetController, 
+    private alertService: AlertService,
   ) {}
 
   ngOnInit() {
@@ -75,70 +71,63 @@ export class ScannerPage implements OnInit {
   }
 
   async submit() {
-    if (
-      !this.member &&
-      (!this.name || !this.address || !this.email || !this.phone)
-    ) {
+    if(!this.name || !this.address || !this.email || !this.phone) {
       const toast = await this.toastController.create({
         showCloseButton: true,
         // cssClass: 'bg-profile',
-        message: "Please Complete the form",
+        message: 'Please Complete the form',
         duration: 3000,
-        position: "bottom"
+        position: 'bottom'
       });
 
       toast.present();
-    } else if (this.serials.length === 0) {
+    } else if(this.serials.length === 0){
       const toast = await this.toastController.create({
         showCloseButton: true,
         // cssClass: 'bg-profile',
-        message: "Please Scan Item First",
+        message: 'Please Scan Item First',
         duration: 3000,
-        position: "bottom"
+        position: 'bottom'
       });
 
       toast.present();
-    } else {
+    }
+     else {
       const loader = await this.loadingController.create({
         duration: 2000
       });
-      this.formData.append("serials", JSON.stringify(this.serials));
-      this.formData.append("name", this.name);
-      this.formData.append("address", this.address);
-      this.formData.append("phone", this.phone);
-      this.formData.append("email", this.email);
-      this.formData.append("user_id", this.member);
+      this.formData.append('serials', JSON.stringify(this.serials));
+      this.formData.append('name', this.name);
+      this.formData.append('address', this.address);
+      this.formData.append('phone', this.phone);
+      this.formData.append('email', this.email);
       this.authService.getToken().then(() => {
         const headers = new HttpHeaders({
-          Authorization:
-            this.authService.token["token_type"] +
-            " " +
-            this.authService.token["access_token"],
+          Authorization: this.authService.token["token_type"] + " " + this.authService.token["access_token"],
           Accept: "application/json"
         });
         loader.present();
         this.http
-          .post(this.env.API_URL + "scanner", this.formData, {
-            headers: headers
-          })
+          .post(this.env.API_URL + "scanner", this.formData, { headers: headers })
           .subscribe(
             data => {
               console.log(data);
               // loader.present();
-              this.storage.remove(STORAGE_KEY);
+              this.storage.remove(STORAGE_KEY)
               loader.onWillDismiss().then(async l => {
                 const toast = await this.toastController.create({
                   showCloseButton: true,
                   // cssClass: 'bg-profile',
-                  message: "Your Application has Been Submmited!",
+                  message: 'Your Application has Been Submmited!',
                   duration: 3000,
-                  position: "bottom"
+                  position: 'bottom'
                 });
 
                 toast.present();
                 this.navCtrl.navigateRoot("/orders");
               });
-              this.images = [];
+              this.images = []
+
             },
             error => {
               console.log(error);
@@ -147,11 +136,9 @@ export class ScannerPage implements OnInit {
                 const toast = await this.toastController.create({
                   showCloseButton: true,
                   // cssClass: 'bg-profile',
-                  message:
-                    "Your Application failed to Submmit!, reasone" +
-                    error.message,
+                  message: 'Your Application failed to Submmit!, reasone' + error.message,
                   duration: 3000,
-                  position: "bottom"
+                  position: 'bottom'
                 });
                 toast.present();
               });
@@ -160,6 +147,7 @@ export class ScannerPage implements OnInit {
       });
     }
   }
+  
 
   loadStoredImages() {
     this.storage.get(STORAGE_KEY).then(images => {
@@ -207,26 +195,20 @@ export class ScannerPage implements OnInit {
   }
 
   copyFileToLocalDir(namePath, currentName, newFileName) {
-    this.file
-      .copyFile(namePath, currentName, this.file.dataDirectory, newFileName)
-      .then(
-        success => {
-          this.updateStoredImages(newFileName);
-        },
-        error => {
-          this.presentToast("Error while storing file.");
-        }
-      );
+    this.file.copyFile(namePath, currentName, this.file.dataDirectory, newFileName).then(success => {
+      this.updateStoredImages(newFileName);
+    }, error => {
+      this.presentToast('Error while storing file.');
+    });
   }
 
   startUpload(imgEntry) {
-    this.file
-      .resolveLocalFilesystemUrl(imgEntry.filePath)
+    this.file.resolveLocalFilesystemUrl(imgEntry.filePath)
       .then(entry => {
-        (<FileEntry>entry).file(file => this.readFile(file));
+        (<FileEntry>entry).file(file => this.readFile(file))
       })
       .catch(err => {
-        this.presentToast("Error while reading file.");
+        this.presentToast('Error while reading file.');
       });
   }
 
@@ -237,7 +219,7 @@ export class ScannerPage implements OnInit {
       const imgBlob = new Blob([reader.result], {
         type: file.type
       });
-      formData.append("file", imgBlob, file.name);
+      formData.append('file', imgBlob, file.name);
       this.uploadImageData(formData);
     };
     reader.readAsArrayBuffer(file);
@@ -245,22 +227,21 @@ export class ScannerPage implements OnInit {
 
   async uploadImageData(formData: FormData) {
     const loading = await this.loadingController.create({
-      message: "Uploading image..."
+      message: 'Uploading image...',
     });
     await loading.present();
 
-    this.http
-      .post("http://localhost:8888/upload.php", formData)
+    this.http.post("http://localhost:8888/upload.php", formData)
       .pipe(
         finalize(() => {
           loading.dismiss();
         })
       )
       .subscribe(res => {
-        if (res["success"]) {
-          this.presentToast("File upload complete.");
+        if (res['success']) {
+          this.presentToast('File upload complete.')
         } else {
-          this.presentToast("File upload failed.");
+          this.presentToast('File upload failed.')
         }
       });
   }
@@ -273,13 +254,10 @@ export class ScannerPage implements OnInit {
       let filtered = arr.filter(name => name != imgEntry.name);
       this.storage.set(STORAGE_KEY, JSON.stringify(filtered));
 
-      var correctPath = imgEntry.filePath.substr(
-        0,
-        imgEntry.filePath.lastIndexOf("/") + 1
-      );
+      var correctPath = imgEntry.filePath.substr(0, imgEntry.filePath.lastIndexOf('/') + 1);
 
       this.file.removeFile(correctPath, imgEntry.name).then(res => {
-        this.presentToast("File removed.");
+        this.presentToast('File removed.');
       });
     });
   }
@@ -318,37 +296,25 @@ export class ScannerPage implements OnInit {
     };
 
     this.camera.getPicture(options).then(imagePath => {
-      if (
-        this.plt.is("android") &&
-        sourceType === this.camera.PictureSourceType.PHOTOLIBRARY
-      ) {
-        this.filePath.resolveNativePath(imagePath).then(filePath => {
-          let correctPath = filePath.substr(0, filePath.lastIndexOf("/") + 1);
-          let currentName = imagePath.substring(
-            imagePath.lastIndexOf("/") + 1,
-            imagePath.lastIndexOf("?")
-          );
-          this.copyFileToLocalDir(
-            correctPath,
-            currentName,
-            this.createFileName()
-          );
-        });
+      if (this.plt.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
+        this.filePath.resolveNativePath(imagePath)
+          .then(filePath => {
+            let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
+            let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
+            this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+          });
       } else {
-        var currentName = imagePath.substr(imagePath.lastIndexOf("/") + 1);
-        var correctPath = imagePath.substr(0, imagePath.lastIndexOf("/") + 1);
-        this.copyFileToLocalDir(
-          correctPath,
-          currentName,
-          this.createFileName()
-        );
+        var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+        var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+        this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
       }
     });
+
   }
 
   pathForImage(img) {
     if (img === null) {
-      return "";
+      return '';
     } else {
       let converted = this.webview.convertFileSrc(img);
       return converted;
@@ -358,23 +324,22 @@ export class ScannerPage implements OnInit {
   async selectImage() {
     const actionSheet = await this.actionSheetController.create({
       header: "Select Image source",
-      buttons: [
-        {
-          text: "Load from Library",
-          handler: () => {
-            this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
-          }
-        },
-        {
-          text: "Use Camera",
-          handler: () => {
-            this.takePicture(this.camera.PictureSourceType.CAMERA);
-          }
-        },
-        {
-          text: "Cancel",
-          role: "cancel"
+      buttons: [{
+        text: 'Load from Library',
+        handler: () => {
+          this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
         }
+      },
+      {
+        text: 'Use Camera',
+        handler: () => {
+          this.takePicture(this.camera.PictureSourceType.CAMERA);
+        }
+      },
+      {
+        text: 'Cancel',
+        role: 'cancel'
+      }
       ]
     });
     await actionSheet.present();
@@ -386,7 +351,7 @@ export class ScannerPage implements OnInit {
       password: "9bAHKVsp!pLdhVxF",
       companyCode: "MY4644"
     };
-
+    
     this.barcodescanner
       .scan()
       .then(barcodeData => {
@@ -407,26 +372,14 @@ export class ScannerPage implements OnInit {
                   headers: headers
                 }
               )
-              .subscribe(
-                data => {
-                  this.serial = data;
-                  console.log("barcode", this.serial.serialNumber);
-                  // if (!this.serials.find(serial => serial === this.serial.serialNumber)) {
-                  //   this.serials.push(this.serial.serialNumber)
-                  // }
-                  this.serials.push({
-                    serial_number: this.serial.serialNumber,
-                    url: this.barcode
-                  });
-                },
-                error => {
-                  console.log("Checknow Error ", error);
-                  this.serials.push({
-                    serial_number: this.barcode,
-                    url: ""
-                  });
-                }
-              );
+              .subscribe(data => {
+                this.serial = data;
+                console.log("barcode", this.serial.serialNumber);
+                // if (!this.serials.find(serial => serial === this.serial.serialNumber)) {
+                //   this.serials.push(this.serial.serialNumber)
+                // }
+                this.serials.push({'serial_number' : this.serial.serialNumber, 'url' :this.barcode})
+              });
           });
       })
       .catch(err => {
@@ -437,7 +390,7 @@ export class ScannerPage implements OnInit {
   async presentToast(text) {
     const toast = await this.toastController.create({
       message: text,
-      position: "bottom",
+      position: 'bottom',
       duration: 3000
     });
     toast.present();
